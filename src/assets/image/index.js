7abdbc5bb814c28ceb0adb46aa02c1edcd918918
DoanLy5120 <doanthaoly.hkqn@gmail.com>
2025-05-22 14:09:51 +0700

    Header phần 1

---
+++ src/assets/image/index.js
@@ -0,0 +1,7 @@
+//tạo 1 object image chứa trường logo 
+//truy cập ảnh này qua image.logo
+const image = {
+    logo: require('~/assets/image/logo.svg').default //import ảnh tĩnh 
+}
+
+export default image;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,36 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-const cx = classNames.bind(styles)
+import styles from './Header.module.scss';
+import { href } from 'react-router-dom';
+import image from '~/assets/image';
+
+const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            {/*logo*/}
-            {/*search*/}
-        </div>
-    </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* logo */}
+                <img src={image.logo} alt="TikTok"/>
+                {/* search */}
+                <div className={cx('search')}>
+                    <input placeholder='Search accounts and video' spellCheck={false}></input>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}/>
+                    </button>
+                    <FontAwesomeIcon  className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
